dc1803c7367a366b54501d4f63a0aca4fd9d54e0
Elad Cohen <elad2.cohen@intel.com>
2017-02-01 11:42:53 +0200

    Add initial program

---
+++ include/hello_world.h
@@ -0,0 +1,4 @@
+#ifndef __HELLO_WORLD_H_
+#define __HELLO_WORLD_H_
+
+#endif /* __HELLO_WORLD_H */
---
+++ src/main.c
@@ -0,0 +1,8 @@
+#include "stdio.h"
+#include "hello_world.h"
+
+int main(){
+  printf("Hello, World!");
+  return 0;
+}
+
